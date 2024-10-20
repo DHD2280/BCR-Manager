45f042e933480efe307dadffaed55bedd5b5881c
DHD2280 <luigiconte98@gmail.com>
2024-10-20 14:37:17 +0200

    Bump deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.6.0" apply false
     id("dev.rikka.tools.materialthemebuilder") version "1.4.1"
 }
 
