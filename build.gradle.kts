16b74214c4bbf5fa2a7b0bc3a53a01d5cd6a9b65
DHD2280 <luigiconte98@gmail.com>
2024-12-04 22:18:14 +0100

    Bump AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.6.0" apply false
+    id("com.android.application") version "8.7.3" apply false
     id("dev.rikka.tools.materialthemebuilder") version "1.4.1"
 }
 
